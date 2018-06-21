7277cdb37db0b27d8d3468480d8fb6663bc47e16
iykequame <izaqkull@yahoo.com>
2018-06-21 07:36:33 +0000

    set TARGET DEVICE

--- omni_X571.mk
+++ omni_X571.mk
@@ -8,7 +8,7 @@ $(call inherit-product, device/Infinix/X571/full_X571.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := X571
-PRODUCT_NAME := full_X571
+PRODUCT_NAME := omni_X571
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix NOTE 4 Pro
 PRODUCT_MANUFACTURER := Infinix
